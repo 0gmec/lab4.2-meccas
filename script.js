4faf383b070084d99b3c5e40a514ed55c12e3c32
mlsfiles75@gmail.com <mlsfiles75@gmail.com>
2025-06-20 19:58:44 -0400

    added task 3 and index.html

--- script.js
+++ script.js
@@ -5,14 +5,14 @@ function addItem(item) {
     if (!shoppingList.includes(item)) {
         shoppingList.push(item);
         console.log(`${item} added to shopping list.`);
-    }else {
+    } else {
         console.log(`${item} is already in the shopping list.`);
     }
 
-    }
-    
+}
 
-function removeLastItem (item) {
+
+function removeLastItem(item) {
     shoppingList.pop(item)
 }
 removeLastItem()
@@ -21,10 +21,10 @@ removeLastItem()
 
 
 function displayList() {
-console.log("Shopping List:");
-for (let i = 0; i < shoppingList.length; i++) {
-    console.log("- " + shoppingList[i]);
-}
+    console.log("Shopping List:");
+    for (let i = 0; i < shoppingList.length; i++) {
+        console.log("- " + shoppingList[i]);
+    }
 }
 
 addItem("soap");
@@ -33,18 +33,50 @@ addItem("tissue");
 addItem("soap");
 addItem("pens");
 
- console.log("Final shopping list:", shoppingList);
+console.log("Final shopping list:", shoppingList);
 displayList();
 
 //TASK 2
 
-function filterItems (searchTerm) {
-    return shoppingList.filter(function(item) {
-return item.toLowerCase().includes(searchTerm.toLowerCase());
+function filterItems(searchTerm) {
+    return shoppingList.filter(function (item) {
+        return item.toLowerCase().includes(searchTerm.toLowerCase());
     });
 
 
 }
-console.log(filterItems("s"));
-console.log(filterItems("pen"));
-console.log(filterItems("onions"));
+// console.log(filterItems("s"));
+// console.log(filterItems("pen"));
+// console.log(filterItems("onions"));
+
+
+//TASK 3    
+
+let myList = [];
+function addItem() {
+    const itemInput = document.getElementById("itemInput");
+    const itemText = itemInput.value.trim();
+
+    if (itemText.length > 0) {
+        myList.push(itemText);
+        itemInput.value = " ";
+
+        const shoppingList = document.getElementById('shoppingList');
+        const listItem = document.createElement('li');
+        listItem.textContent = itemText;
+        shoppingList.appendChild(listItem);
+    }
+}
+    function removeItem() {
+        if (myList.length> 0) {
+            myList.pop();
+            const shoppingList = document.getElementById('shoppingList');
+            shoppingList.removeChild(shoppingList.lastChild);
+        }
+
+    }
+
+
+
+
+
