7d42f3fc47ad14f8d293331bf30b5af7c0347504
mlsfiles75@gmail.com <mlsfiles75@gmail.com>
2025-06-20 18:55:29 -0400

    added task two

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
-//TASK
+//TASK 1
 
-let shoppingList = ["soap","pens","tissue"];
+let shoppingList = [];
 function addItem(item) {
-    shoppingList.push(item);
-}
-addItem("soap")
-addItem("pens")
-addItem("tissue")
+    if (!shoppingList.includes(item)) {
+        shoppingList.push(item);
+        console.log(`${item} added to shopping list.`);
+    }else {
+        console.log(`${item} is already in the shopping list.`);
+    }
 
-//console.log(shoppingList);
+    }
+    
 
 function removeLastItem (item) {
     shoppingList.pop(item)
@@ -24,4 +26,25 @@ for (let i = 0; i < shoppingList.length; i++) {
     console.log("- " + shoppingList[i]);
 }
 }
-displayList()
+
+addItem("soap");
+addItem("pens");
+addItem("tissue");
+addItem("soap");
+addItem("pens");
+
+ console.log("Final shopping list:", shoppingList);
+displayList();
+
+//TASK 2
+
+function filterItems (searchTerm) {
+    return shoppingList.filter(function(item) {
+return item.toLowerCase().includes(searchTerm.toLowerCase());
+    });
+
+
+}
+console.log(filterItems("s"));
+console.log(filterItems("pen"));
+console.log(filterItems("onions"));
